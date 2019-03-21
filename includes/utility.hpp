1c3f5749506cbb96803c67c3ed42cd7df63abb6e
linofex <alessandro.noferi@gmail.com>
2019-03-21 10:27:03 +0100

    comment on utility.hpp

--- includes/utility.hpp
+++ includes/utility.hpp
@@ -1,8 +1,8 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-typedef unsigned char BYTE;
-typedef unsigned short WORD;
+typedef unsigned char BYTE; // 8bit
+typedef unsigned short WORD; // 16bit
 
 #if defined MEMORY_H
 #define KB 0x400  // 1024B

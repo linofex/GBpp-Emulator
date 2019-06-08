1161985313e28ec26c20a6805ccedcfe28b4eadb
linofex <alessandro.noferi@gmail.com>
2019-06-08 10:13:56 +0200

    deleted old commented code

--- sources/interrupts.cpp
+++ sources/interrupts.cpp
@@ -10,7 +10,6 @@ void InterruptHandler::serveInterrupt(const BYTE t_interrupt, Memory* t_memory, 
     t_cpu->pushWord(t_cpu->getPC());  // Save PC in the stack
 
     BYTE irr = t_memory->readByte(IRR_ADD);          // Read Interrupt Request Reister from memory at 0x0FF0F
-    //std::cout << "Lo tolgo:";
     t_memory->writeByte(IRR_ADD, irr ^ t_interrupt); // Reset the Interrupet flag to 0, since is served
     
      
@@ -34,23 +33,18 @@ void InterruptHandler::serveInterrupt(const BYTE t_interrupt, Memory* t_memory, 
             t_cpu->setPC(JOYPAD_ADD);
             t_cpu->resetStop();
     }
+    t_cpu->addClockCycles(12);
 }
 
 bool InterruptHandler::doInterrupt(Memory* t_memory, Cpu* t_cpu){
-//        std::cerr << "DIS";
     BYTE ier = t_memory->readByte(IER_ADD); //Read Interrupt Enable Reister from memory at 0x0FFFF
     BYTE irr = t_memory->readByte(IRR_ADD); //Read Interrupt Request Reister from memory at 0x0FF0F
-        
     if (t_cpu->isIntMasterEnable()){
-        // if(t_cpu->getInstrSetAt(t_cpu->getPC()).name == "HALT"){
-        //     t_cpu->incPC();
-        // }
-
         /* If the interrupt is enabled AND the interrupt is requested --> ServeInterrupt */
         /*          IN ORDER OF PRIORITY            */
-        if(ier & VBLANK & irr)  {/* std::cout << "INT\n"; */    serveInterrupt(VBLANK, t_memory, t_cpu); return true;} // Higher priority
-        if(ier & LCD & irr)     {/* std::cout << "INT\n"; */    serveInterrupt(LCD, t_memory, t_cpu); return false;}
-        if(ier & TIMER & irr)   {/*std::cout << "INT\n";*/      serveInterrupt(TIMER, t_memory, t_cpu); return false;}
+        if(ier & VBLANK & irr)  {/* std::cout << "INT\n";  */   serveInterrupt(VBLANK, t_memory, t_cpu); return true;} // Higher priority
+        if(ier & LCD & irr)     {/* std::cout << "INT\n";  */   serveInterrupt(LCD, t_memory, t_cpu); return false;}
+        if(ier & TIMER & irr)   {/*std::cout << "INT\n";   */   serveInterrupt(TIMER, t_memory, t_cpu); return false;}
         if(ier & JOYPAD & irr)  {/*std::cerr << "JOYPAD\n";*/   serveInterrupt(JOYPAD, t_memory, t_cpu); return false;}   
     }
     else{
@@ -63,6 +57,6 @@ bool InterruptHandler::doInterrupt(Memory* t_memory, Cpu* t_cpu){
 }
 
 void InterruptHandler::requestInterrupt(Memory* t_mem, const BYTE t_interrupt){
-    BYTE irr = t_mem->readByte(IRR_ADD);          // Read Interrupt Request Reister from memory at 0x0FF0F
+    BYTE irr = t_mem->readByte(IRR_ADD);          // Read Interrupt Request Register from memory at 0x0FF0F
     t_mem->writeByte(IRR_ADD, irr | t_interrupt); // Set the Interrupt flag to 1, since requested    
 }

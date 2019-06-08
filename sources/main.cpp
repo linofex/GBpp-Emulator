cc23594f15c3f1e3287239e6fdbda11ace2f92dc
linofex <alessandro.noferi@gmail.com>
2019-06-08 10:16:29 +0200

    deleted unuseful code

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,28 +1,23 @@
-//#include "../includes/rom.hpp"
-//#include "../includes/memory.hpp"
 #include "../includes/GameBoy.hpp"
 #include "../includes/utility.hpp"
 
-//#include "../includes/cpu.hpp"
-//using namespace GBemulator;
-
+ // used to run tetris faster
 bool isTetris = false;
 
 int main(int argc, char** argv){
     SDL_SetMainReady();
     if (argc == 2){
         std::string romName = "./roms/" + std::string(argv[1]);
-        if (std::string(argv[1]) == "tetris.gb"){ isTetris = true;}
+        if (std::string(argv[1]) == "tetris.gb"){isTetris = true;}
         GameBoy gameboy = GameBoy(romName);
+        gameboy.boot();
         if(gameboy.loadGame()){
-            gameboy.boot();
             gameboy.playGame();
         }
         else{
             gameboy.turnOff();
         }
     }
-
     return 0;    
 
 } 
